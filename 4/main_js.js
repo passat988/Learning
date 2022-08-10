ac7b701f29395ec6def380fd2a87adc6153e5f5c
passat988 <107483813+passat988@users.noreply.github.com>
2022-08-11 01:17:24 +0300

    js without "favourite items"

--- 4/main_js.js
+++ 4/main_js.js
@@ -1,38 +1,42 @@
 document.body.onclick = (event) => {
-    const number = document.querySelector('.phone-number')
-    const copyImg = event.target;
-    if (number.classList.contains('phone-number')  || copyImg.classList.contains('copy-img')) {
-        navigator.clipboard.writeText(number.innerHTML);
-        
-    }
-}
+  const number = document.querySelector(".phone-number");
+  const copyImg = event.target;
+  if (
+    number.classList.contains("phone-number") ||
+    copyImg.classList.contains("copy-img")
+  ) {
+    navigator.clipboard.writeText(number.innerHTML);
+  }
+};
 
-const visibleBlock = function() {
-    modal.style.display = "block";
-}
-const invisibleBlock = function() {
-    modal.style.display = "none";
-}
+const copyControl = document.querySelector(".number-and-img");
+const copyToast = document.querySelector(".copyed");
 
+copyControl.addEventListener("click", () => {
+  copyToast.classList.add("copyed_active");
 
+  setTimeout(() => {
+    copyToast.classList.remove("copyed_active");
+  }, 2000);
+});
 
-// document.addEventListener('touchstart', handleTouchStart, false);     
+// document.addEventListener('touchstart', handleTouchStart, false);
 // document.addEventListener('touchmove', handleTouchMove, false);
 
-// let xDown = null;                                                        
-// let yDown = null;                                                        
+// let xDown = null;
+// let yDown = null;
 
-// function handleTouchStart(evt) {                                         
-//     xDown = evt.touches[0].clientX;                                      
-//     yDown = evt.touches[0].clientY;                                      
-// };                                                
+// function handleTouchStart(evt) {
+//     xDown = evt.touches[0].clientX;
+//     yDown = evt.touches[0].clientY;
+// };
 
 // function handleTouchMove(evt) {
 //     if ( ! xDown || ! yDown ) {
 //         return;
 //     }
 
-//     let xUp = evt.touches[0].clientX;                                    
+//     let xUp = evt.touches[0].clientX;
 //     let yUp = evt.touches[0].clientY;
 
 //     let xDiff = xDown - xUp;
@@ -42,74 +46,88 @@ const invisibleBlock = function() {
 //             document.location.href = "./index2.html"
 //         } else {
 //             document.location.href = "./index3.html"
-//         }                       
-//     } 
-    
+//         }
+//     }
+
 //     xDown = null;
-//     yDown = null;                                             
+//     yDown = null;
 // };
 
-document.addEventListener('touchstart', handleTouchStart, false);     
-document.addEventListener('touchmove', handleTouchMove, false);
+document.addEventListener("touchstart", handleTouchStart, false);
+document.addEventListener("touchmove", handleTouchMove, false);
 
-let xDown = null;                                                        
-let yDown = null;                                                        
+let xDown = null;
+let yDown = null;
 
-function handleTouchStart(evt) {                                         
-    xDown = evt.touches[0].clientX;                                      
-    yDown = evt.touches[0].clientY;                                      
-};                                                
+function handleTouchStart(evt) {
+  xDown = evt.touches[0].clientX;
+  yDown = evt.touches[0].clientY;
+}
 
 let curUrl = document.URL;
 
-const pagesArr =[ "", 2, 3, 4 ]
+const pagesArr = ["", 2, 3, 4];
 
-let leftswipe = function(){
-    for ( let i =1; i< pagesArr.length; i++){
-        if(curUrl.indexOf("file:///C:/Users/User/Documents/GitHub/Learning/4/index"+ pagesArr[i] +".html")!=-1){ 
-            document.location.href = "./index"+ pagesArr[i+1] +".html"
-         }   
+let leftswipe = function () {
+  for (let i = 1; i < pagesArr.length; i++) {
+    if (
+      curUrl.indexOf(
+        "file:///C:/Users/User/Documents/GitHub/Learning/4/index" +
+          pagesArr[i] +
+          ".html"
+      ) != -1
+    ) {
+      document.location.href = "./index" + pagesArr[i + 1] + ".html";
     }
-}
-
-let rightswipe = function(){
-    for ( let i =1; i< pagesArr.length; i++){
-        if(curUrl.indexOf("file:///C:/Users/User/Documents/GitHub/Learning/4/index"+ pagesArr[i] +".html")!=-1){ 
-            document.location.href = "file:///C:/Users/User/Documents/GitHub/Learning/4/index"+ pagesArr[i-1] +".html"
-         }   
-    }
-}
-
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
-
-    let xUp = evt.touches[0].clientX;                                    
-    let yUp = evt.touches[0].clientY;
-
-    let xDiff = xDown - xUp;
-    let yDiff = yDown - yUp;
-
-
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
-        if ( xDiff > 0 ) {
-            leftswipe()
-        } else {
-            rightswipe()
-        }                       
-    } 
-    
-    xDown = null;
-    yDown = null;                                             
+  }
 };
 
-const iconMenu = document.querySelector('.mobile-menu')
-if (iconMenu){
-    const menuBody = document.querySelector('.navigation')
-    iconMenu.addEventListener("click", function(e) {
-        document.body.classList.toggle('_lock')
-        iconMenu.classList.toggle('_active')
-        menuBody.classList.toggle('_active')
-    })
+let rightswipe = function () {
+  for (let i = 1; i < pagesArr.length; i++) {
+    if (
+      curUrl.indexOf(
+        "file:///C:/Users/User/Documents/GitHub/Learning/4/index" +
+          pagesArr[i] +
+          ".html"
+      ) != -1
+    ) {
+      document.location.href =
+        "file:///C:/Users/User/Documents/GitHub/Learning/4/index" +
+        pagesArr[i - 1] +
+        ".html";
+    }
+  }
+};
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  let xUp = evt.touches[0].clientX;
+  let yUp = evt.touches[0].clientY;
+
+  let xDiff = xDown - xUp;
+  let yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    if (xDiff > 0) {
+      leftswipe();
+    } else {
+      rightswipe();
+    }
+  }
+
+  xDown = null;
+  yDown = null;
+}
+
+const iconMenu = document.querySelector(".mobile-menu");
+if (iconMenu) {
+  const menuBody = document.querySelector(".navigation");
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+  });
 }
--- 4/modal.js
+++ 4/modal.js
@@ -18,3 +18,25 @@ const missClick = function(event) {
 btn.addEventListener( 'click', visibleBlock, false )
 span.addEventListener( 'click', invisibleBlock, false )
 document.addEventListener( 'click', missClick, false )
+
+let form = document.querySelector('form');
+
+document.addEventListener('submit', function (event) {
+
+  	event.preventDefault();
+ 	let url = 'https://jsonplaceholder.typicode.com/posts';
+
+  	fetch(url, {
+ 		  method: 'POST',
+ 		  body: new URLSearchParams(Array.from(new FormData(form))),
+    }).then(function (response) {
+ 			if (response.ok) {
+ 				return response.json();
+        	}
+ 	   return Promise.reject(response);
+    }).then(function (formData) {
+ 	   console.log(formData);
+    }).catch(function (error) {
+       console.warn(error);
+    });
+});
